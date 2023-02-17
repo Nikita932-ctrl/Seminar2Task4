1314a475a16b5a35a1e5a0edf6c3483125734469
Nikita932-ctrl <65113631+Nikita932-ctrl@users.noreply.github.com>
2023-02-17 21:23:06 +0300

    Create Seminar2Task4.cs

---
+++ Seminar2Task4.cs
@@ -0,0 +1,50 @@
+﻿
+// Эта часть кода робить , как надо не хочет ,дайте подсказку ,почему ,пожалуйста),выдаётся другое число
+
+Console.WriteLine("Введите день недели: ");
+int days = Convert.ToInt32(Console.ReadLine()); //Изменил  Console.Read() на Console.ReadLine() и все заработало,почему так ,не подскажете? :)
+int i = 0;
+while (true)
+{
+    if (days == 0)
+    {
+        break;
+    }
+    else if (days > 5)
+    {
+        Console.WriteLine($"День недели {days} выходной");
+        break;
+
+    }
+    else if (days <= 5)
+    {
+        Console.WriteLine($"День недели {days} не выходной");
+        break;
+    }
+}
+
+
+string daysOfWeekend (int days)
+{
+
+    if (days > 5)
+    {
+        Console.WriteLine($"День недели {days} выходной");
+
+
+    }
+    else if (days <= 5)
+    {
+        Console.WriteLine($"День недели {days} не выходной");
+
+    }
+    else if (days < 1 && days > 7)
+    {
+        Console.WriteLine("Нет такого дня недели");
+    }
+    return "end";
+
+
+}
+
+daysOfWeekend (0);
